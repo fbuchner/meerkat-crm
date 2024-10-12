ecf0cd1beba3c835dacedd9bfd55a2b251c44a6d
fredericbnr <25030946+fbuchner@users.noreply.github.com>
2024-10-12 21:53:41 +0200

    Added stylesheet

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Import the router
+import './assets/styles.css';  // Import global CSS
 
 const app = createApp(App);
 
 app.use(router); // Use the router in your app
 app.mount('#app');
+

2c370f712fd02a11a8a4bdad5326a56360a273d9
fredericbnr <25030946+fbuchner@users.noreply.github.com>
2024-11-01 19:36:16 +0100

    Fixed birthday date entry (also w/o year)

--- frontend/src/components/ContactAdd.vue
+++ frontend/src/components/ContactAdd.vue
@@ -6,125 +6,52 @@
           <v-card-title>Add New Contact</v-card-title>
           <v-card-text>
             <v-form @submit.prevent="submitForm">
-              <v-text-field
-                label="First Name"
-                v-model="contact.firstname"
-                required
-              ></v-text-field>
+              <v-text-field label="First Name" v-model="contact.firstname" required></v-text-field>
 
-              <v-text-field
-                label="Last Name"
-                v-model="contact.lastname"
-              ></v-text-field>
+              <v-text-field label="Last Name" v-model="contact.lastname"></v-text-field>
 
-              <v-text-field
-                label="Nickname"
-                v-model="contact.nickname"
-              ></v-text-field>
+              <v-text-field label="Nickname" v-model="contact.nickname"></v-text-field>
 
-              <v-select
-                label="Gender"
-                v-model="contact.gender"
-                :items="['Male', 'Female', 'Unknown']"
-              ></v-select>
+              <v-select label="Gender" v-model="contact.gender" :items="['Male', 'Female', 'Unknown']"></v-select>
 
-              <v-text-field
-                label="Circles"
-                v-model="circleInput"
-                @keyup.space="addCircle"
-                placeholder="Add a circle and press Space"
-              ></v-text-field>
+              <v-text-field label="Circles" v-model="circleInput" @keyup.space="addCircle"
+                placeholder="Add a circle and press Space"></v-text-field>
 
               <v-chip-group v-if="contact.circles.length">
-                <v-chip
-                  v-for="(circle, index) in contact.circles"
-                  :key="index"
-                  close
-                  @click:close="removeCircle(index)"
-                >
+                <v-chip v-for="(circle, index) in contact.circles" :key="index" close
+                  @click:close="removeCircle(index)">
                   {{ circle }}
                 </v-chip>
               </v-chip-group>
 
-              <v-text-field
-                label="Email"
-                v-model="contact.email"
-                type="email"
-              ></v-text-field>
+              <v-text-field label="Email" v-model="contact.email" type="email"></v-text-field>
+
+              <v-text-field label="Phone" v-model="contact.phone" type="tel"></v-text-field>
 
               <v-text-field
-                label="Phone"
-                v-model="contact.phone"
-                type="tel"
+                label="Birthday"
+                v-model="birthdayInput"
+                placeholder="DD.MM.YYYY (year optional)"
+                :error-messages="birthdayError"
+                @blur="validateBirthday"
               ></v-text-field>
 
-              <v-menu
-                ref="menu"
-                v-model="menu"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="formattedBirthday"
-                    label="Birthday"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker 
-                  v-model="contact.birthday" 
-                  no-title 
-                  @input="updateBirthday"
-                ></v-date-picker>
-              </v-menu>
+              <v-text-field label="Address" v-model="contact.address"></v-text-field>
 
+              <v-textarea label="How We Met" v-model="contact.how_we_met"></v-textarea>
 
-              <v-text-field
-                label="Address"
-                v-model="contact.address"
-              ></v-text-field>
+              <v-text-field label="Food Preference" v-model="contact.food_preference"></v-text-field>
 
-              <v-textarea
-                label="How We Met"
-                v-model="contact.how_we_met"
-              ></v-textarea>
+              <v-text-field label="Work Information" v-model="contact.work_information"></v-text-field>
 
-              <v-text-field
-                label="Food Preference"
-                v-model="contact.food_preference"
-              ></v-text-field>
-
-              <v-text-field
-                label="Work Information"
-                v-model="contact.work_information"
-              ></v-text-field>
-
-              <v-textarea
-                label="Additional Contact Information"
-                v-model="contact.contact_information"
-              ></v-textarea>
+              <v-textarea label="Additional Contact Information" v-model="contact.contact_information"></v-textarea>
 
               <v-btn type="submit" color="primary">Add Contact</v-btn>
 
-              <v-alert
-                v-if="successMessage"
-                type="success"
-                dismissible
-                class="mt-3"
-              >
+              <v-alert v-if="successMessage" type="success" dismissible class="mt-3">
                 {{ successMessage }}
               </v-alert>
-              <v-alert
-                v-if="errorMessage"
-                type="error"
-                dismissible
-                class="mt-3"
-              >
+              <v-alert v-if="errorMessage" type="error" dismissible class="mt-3">
                 {{ errorMessage }}
               </v-alert>
             </v-form>
@@ -148,7 +75,7 @@ export default {
         gender: 'Unknown',
         email: '',
         phone: '',
-        birthday: '', // stores the selected date in YYYY-MM-DD format
+        birthday: null,
         address: '',
         how_we_met: '',
         food_preference: '',
@@ -159,12 +86,18 @@ export default {
       successMessage: '',
       errorMessage: '',
       circleInput: '',
-      menu: false, // controls the open/close state of the date picker menu
-      formattedBirthday: '', // formatted birthday for display
+      birthdayInput: '', 
+      birthdayError: '', 
+   
     };
   },
   methods: {
     submitForm() {
+      this.validateBirthday();
+      if (this.birthdayError) {
+        this.errorMessage = "Please correct the birthday format.";
+        return;
+      }
       contactService
         .addContact(this.contact)
         .then(() => {
@@ -204,18 +137,23 @@ export default {
         circles: [],
       };
       this.circleInput = '';
-      this.formattedBirthday = '';
     },
-    updateBirthday(date) {
-      // Update the contact's birthday and format it for display
-      this.contact.birthday = date;
-      this.formattedBirthday = this.formatDate(date);
-      this.menu = false;
+    validateBirthday() {
+      // Regular expression to match "DD.MM.YYYY" or "DD.MM." format
+      const datePattern = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})?$/;
+      if (!this.birthdayInput.match(datePattern)) {
+        this.birthdayError = "Please enter a valid date in DD.MM.YYYY or DD.MM. format.";
+      } else {
+        this.birthdayError = '';
+        // Convert input to the YYYY-MM-DD format, setting the year to 0001 if omitted
+        this.formatBirthday();
+      }
     },
-    formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      return new Date(date).toLocaleDateString(undefined, options);
+    formatBirthday() {
+      const [day, month, year] = this.birthdayInput.split('.');
+      this.contact.birthday = `${year || '0001'}-${month}-${day}`;
     },
+
   },
 };
 </script>

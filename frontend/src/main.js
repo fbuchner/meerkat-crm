59be4c27ca49a68a796dd5b6171429d143ae22d5
fredericbnr <25030946+fbuchner@users.noreply.github.com>
2024-12-07 15:05:46 +0100

    Add I18N capabilities

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,30 @@
 // src/main.js
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
+import { loadLocaleMessages } from './locales/index';
 import App from './App.vue';
-import router from './router'; // Import the router
-import 'vuetify/styles' // Ensure you are using CSS styles
-import vuetify from './plugins/vuetify' // Import Vuetify
+import router from './router';
+import 'vuetify/styles';
+import vuetify from './plugins/vuetify';
+
+const i18n = createI18n({
+    legacy: false, 
+    locale: 'en',  // Default language
+    fallbackLocale: 'en', 
+    messages: {}
+  });
+
+const app = createApp(App);
+
+(async () => {
+const savedLanguage = localStorage.getItem('preferredLanguage') || 'en';
+await loadLocaleMessages(i18n, savedLanguage); 
+app.use(i18n)
+app.use(router) 
+app.use(vuetify) 
+app.mount('#app');
+})();
 
 
-createApp(App)
-    .use(router) // Use the router in your app
-    .use(vuetify) // Add Vuetify as a plugin
-    .mount('#app');
+
 

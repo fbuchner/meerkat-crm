9371877df951c9e8fe3a4bcffc949bcee09301ac
fredericbnr <25030946+fbuchner@users.noreply.github.com>
2024-12-08 20:07:53 +0100

    Further decoupling of timeline

--- frontend/src/components/ContactView.vue
+++ frontend/src/components/ContactView.vue
@@ -94,57 +94,21 @@
 
             <!-- Right Column: Timeline for Notes and Activities -->
             <v-col cols="12" md="8">
-                <v-card outlined>
-                    <v-card-title>
-                        {{ $t('contacts.timeline') }}
-                        <v-spacer></v-spacer>
-                        <v-btn @click="openAddNote" color="primary" density="compact"
-                            prepend-icon="mdi-note-plus-outline"> {{
-                            $t('notes.add_note') }}</v-btn>
-                        <v-btn @click="openAddActivity" color="primary" density="compact"
-                            prepend-icon="mdi-account-multiple-plus-outline" class="ml-2">{{
-                                $t('activities.add_activity')
-                            }}</v-btn>
-                    </v-card-title>
-                    <v-card-text>
-                        <v-col cols="12" md="8">
-                            <Timeline :timelineItems="sortedTimelineItems" @addNote="openAddNote"
-                                @addActivity="openAddActivity" @editActivity="editActivity"
-                                @deleteActivity="deleteActivity" @editNote="editNote" @deleteNote="deleteNote" />
-                        </v-col>
-
-                    </v-card-text>
-                </v-card>
+                <ContactTimeline :timelineItems="sortedTimelineItems" :contactId="contact.ID"
+                    @refreshTimeline="fetchContact" />
             </v-col>
+
         </v-row>
-
-        <!-- Dialog Modals for Adding Activity and Note -->
-        <v-dialog v-model="showAddActivity" max-width="500px" persistent>
-            <ActivityAdd :contactId="contact.ID" :activityId="editingActivityId"
-                :initialActivity="editingActivityData || {}" @activityAdded="refreshContact"
-                @close="showAddActivity = false" />
-        </v-dialog>
-
-        <v-dialog v-model="showAddNote" max-width="500px" persistent>
-            <NoteAdd :contactId="contact.ID" :noteId="editingNoteId" :initialNote="editingNoteData || {}"
-                @noteAdded="refreshContact" @close="showAddNote = false" />
-        </v-dialog>
-
-
-
-
     </v-container>
 </template>
 
 <script>
 import contactService from '@/services/contactService';
 import { reactive } from 'vue';
-import ActivityAdd from '@/components/ActivityAdd.vue';
-import NoteAdd from '@/components/NoteAdd.vue';
 import ProfilePhoto from './ProfilePhoto.vue';
 import activityService from '@/services/activityService';
-import noteService from '@/services/noteService';
 import RelationshipList from '@/components/RelationshipList.vue';
+import ContactTimeline from '@/components/ContactTimeline.vue';
 
 export default {
     name: 'ContactView',
@@ -153,12 +117,10 @@ export default {
             required: true,
         },
     },
-    components: { ActivityAdd, NoteAdd, ProfilePhoto, RelationshipList },
+    components: { ProfilePhoto, RelationshipList, ContactTimeline },
     data() {
         return {
             contact: null,
-            showAddActivity: false,
-            showAddNote: false,
             isEditing: reactive({}),
             editValues: reactive({}),
             isEditingName: false,
@@ -286,27 +248,6 @@ export default {
             this.isEditingName = false;
             this.editName = `${this.contact.firstname} ${this.contact.lastname}`;
         },
-        openAddActivity() {
-            this.editingActivityId = null;
-            this.editingActivityData = {};
-            this.showAddActivity = true;
-        },
-        openAddNote() {
-            this.editingNoteId = null;
-            this.editingNoteData = {};
-            this.showAddNote = true;
-        },
-        async editActivity(activityId) {
-            const activity = this.contact.activities.find((a) => a.ID === activityId);
-            this.editingActivityId = activityId;
-            this.editingActivityData = {
-                title: activity.title,
-                description: activity.description,
-                date: activity.date,
-                location: activity.location,
-            };
-            this.showAddActivity = true;
-        },
         async deleteContact() {
             try {
                 await contactService.deleteContact(this.contact.ID);
@@ -324,31 +265,8 @@ export default {
                 console.error('Error deleting activity:', error);
             }
         },
-        async editNote(noteId) {
-            const note = this.contact.notes.find((n) => n.ID === noteId);
-            this.editingNoteId = noteId;
-            this.editingNoteData = {
-                content: note.content,
-                date: note.date,
-            };
-            this.showAddNote = true;
-        },
-        async deleteNote(noteId) {
-            try {
-                await noteService.deleteNote(noteId);
-                this.refreshContact(); // Refresh contact details after deletion
-            } catch (error) {
-                console.error('Error deleting note:', error);
-            }
-        },
         // Refresh the contact details after adding, editing, or deleting
         refreshContact() {
-            this.showAddActivity = false;
-            this.showAddNote = false;
-            this.editingActivityId = null;
-            this.editingActivityData = null;
-            this.editingNoteId = null;
-            this.editingNoteData = null;
             this.fetchContact();
         },
         // Toggle the add circle input visibility
@@ -446,9 +364,8 @@ export default {
 }
 
 .field-label:hover .edit-icon,
-.field-label:hover .delete-icon,
-.timeline-date-section:hover .edit-icon,
-.timeline-date-section:hover .delete-icon {
+.field-label:hover .delete-icon
+{
     opacity: 1;
     /* Show icons on hover */
 }
